e766fad1b0bd6ae1de0262fef41ca7a7061a0d5d
angrykoala <angrykoala@outlook.es>
2022-04-20 09:55:58 +0100

    basic example

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# neo4j-grapql-learning-session
-A learning session of neo4j/graphql demo
+# Neo4j/GraphQL continuous learning session demo
+
+This is the demo material of the continuous learning session of the `@neo4j/graphql` library held the 20th of April, 2022.
+
+
+## Demos
+
+### Technical dive into library setup and usage
+The file `basic-test.js` sets a basic [Apollo server](apollographql.com/docs/apollo-server/) with [@neo4j/graphql](https://www.npmjs.com/package/@neo4j/graphql)
+
+1. `node basic-test.js`
+2. Got to `http://localhost:4000/`
+
+2. Subscriptions Demo
+3. Toolbox Demo
---
+++ basic-test.js
@@ -0,0 +1,29 @@
+const { Neo4jGraphQL } = require("@neo4j/graphql");
+const { ApolloServer, gql } = require("apollo-server");
+const neo4j = require("neo4j-driver");
+
+const typeDefs = gql`
+    type Movie {
+        title: String
+        actors: [Actor!]! @relationship(type: "ACTED_IN", direction: IN)
+    }
+
+    type Actor {
+        name: String
+        movies: [Movie!]! @relationship(type: "ACTED_IN", direction: OUT)
+    }
+`;
+
+const driver = neo4j.driver("bolt://localhost:7687", neo4j.auth.basic("neo4j", "dontpanic42"));
+
+const neoSchema = new Neo4jGraphQL({ typeDefs, driver });
+
+neoSchema.getSchema().then((schema) => {
+  const server = new ApolloServer({
+      schema,
+  });
+
+  server.listen().then(({ url }) => {
+      console.log(`🚀 Server ready at ${url}`);
+  });
+})
